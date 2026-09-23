745837ed855e8775d50aad8396dac89a94382ea8
BraidaFac <facundobraida98@gmail.com>
2026-09-23 17:28:16 +1200

    clear project

--- booking-app/src/lib/user.controller.ts
+++
@@ -1,9 +0,0 @@
-import { prisma } from "./prisma";
-
-export async function getUserByUsername(username: string) {
-  return await prisma.user.findFirst({
-    where: {
-      username: username.toLowerCase(),
-    },
-  });
-}
